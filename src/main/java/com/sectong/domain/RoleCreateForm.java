268abcc161c0b3b50d4b9e1f28cb396b2a5cce33
zhengyunfei <zhengyf@ctyun.cn>
2017-03-28 14:32:28 +0800

    完善角色管理功能

---
+++ src/main/java/com/sectong/domain/RoleCreateForm.java
@@ -0,0 +1,60 @@
+package com.sectong.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * 创建角色字段POJO定义
+ * @author:郑云飞
+ * @createDate:2017-03-28
+ *
+ */
+public class RoleCreateForm {
+
+	@NotEmpty
+	private String roleno;
+
+	@NotEmpty
+	private String rolename;
+	@NotEmpty
+	private int isactive;
+	@NotEmpty
+	private String roledesc;
+
+	public String getRoleno() {
+		return roleno;
+	}
+
+	public void setRoleno(String roleno) {
+		this.roleno = roleno;
+	}
+
+	public String getRolename() {
+		return rolename;
+	}
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+
+	public int getIsactive() {
+		return isactive;
+	}
+
+	public void setIsactive(int isactive) {
+		this.isactive = isactive;
+	}
+
+	public String getRoledesc() {
+		return roledesc;
+	}
+
+	public void setRoledesc(String roledesc) {
+		this.roledesc = roledesc;
+	}
+
+	@Override
+	public String toString() {
+		return "UserCreateForm [roleNo=" + roleno + ", rolename=" + rolename + "]";
+	}
+
+}
